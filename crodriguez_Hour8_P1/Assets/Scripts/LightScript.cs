7296bb3dcd913faed6c4f82366d06c816cc59d68
reyk93 <citlalirgonz@gmail.com>
2025-10-20 09:52:45 -0700

    Made some changes to Hour8_P1 Light script and tried other way to write the light enable code

--- crodriguez_Hour8_P1/Assets/Scripts/LightScript.cs
+++ crodriguez_Hour8_P1/Assets/Scripts/LightScript.cs
@@ -4,45 +4,27 @@ using UnityEngine;
 
 public class LightScript : MonoBehaviour
 {
+
+    Light _lightbulb;
+
     // Start is called before the first frame update
-
-    Light myLight;
-
-   
     void Start()
     {
-
-        myLight = GetComponent<Light>();
-        //bool isOn = false; --- other option in progress
-
+        _lightbulb = gameObject.GetComponent<Light>();
 
     }
-
-    
 
     // Update is called once per frame
     void Update()
     {
 
-            if (Input.GetKeyDown(KeyCode. L)) 
+        if (Input.GetKeyDown(KeyCode.L)) 
         {
-           
             Debug.Log("The 'L' key is pressed down");
-            myLight.enabled = !myLight.enabled;
-            
+            _lightbulb.enabled = !_lightbulb.enabled;   // = ! this means opositive of the first, this allow to enable and disable an object in unity 
+
+        
         }
 
-            //  other option below using if /else 
-
-       
-        
-        
-
     }
-
-    
-
-
-
-
 }
